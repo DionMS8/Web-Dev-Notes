2d8a93f2445ade019f6db13533025e0d3e69ad67
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2021-11-13 16:56:49 -0400

    Created a React Counter

--- REACT/counter.js
+++ REACT/counter.js
@@ -2,10 +2,18 @@ import React, { useState } from "react"
 import ReactDOM from "react-dom"
 
 export default function App() {
-    
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <h1>My count is { count }</h1>
+            <button onClick={() =>setCount(count+1)}>
+                Add Count
+            </button>
+        </div>
+    );
 }
 
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
