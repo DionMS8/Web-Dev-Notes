18eed2f40eb1ebaa4a1ea0f374f798bd8e7f2b24
Dion Singh <55777067+DionMS8@users.noreply.github.com>
2022-01-03 03:37:34 -0400

    Added Notes on Mongoose for MongoDB

---
+++ NODE/requests.js
@@ -0,0 +1,19 @@
+// HTTP REQUESTS WITH NODE.JS
+
+// USING AXIOS
+
+const axios = require("axios");
+const url = "<a href="https://my-json-server.typicode.com/edurekaDemo/noderequest/db">https://my-json-server.typicode.com/edurekaDemo/noderequest/db</a>";
+const getData = async url => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getData(url);
+
+
+
